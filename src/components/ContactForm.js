f8843ff543e2c822e2f8d9e061e296f785054f4a
BkAngel201 <33973377+BkAngel201@users.noreply.github.com>
2020-07-17 00:37:55 -0400

    nvp for testing and half stretch

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import axios from "axios"
 
 const ContactForm = () => {
   const [data, setData] = useState();
@@ -7,18 +8,22 @@ const ContactForm = () => {
     mode: "onBlur",
   });
   const onSubmit = (data) => {
-    setData(data);
+    axios.post('https://reqres.in/api/users',data)
+    .then(res => setData(res.data))
+    
+
   };
 
   return (
     <div className="App">
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit)} data-testid="form">
         <div>
           <label htmlFor="firstName">First Name*</label>
           <input
             name="firstName"
+            data-testid = "first-name"
             placeholder="Edd"
-            ref={register({ required: true, maxLength: 3 })}
+            ref={register({ required: true, maxLength: 10 })}
           />
           {errors.firstName && (
             <p>Looks like there was an error: {errors.firstName.type}</p>
@@ -29,6 +34,7 @@ const ContactForm = () => {
           <label htmlFor="lastName">Last Name*</label>
           <input
             name="lastName"
+            data-testid = "last-name"
             placeholder="Burke"
             ref={register({ required: true })}
           />
@@ -41,21 +47,21 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input data-testid="email" name="email" ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea data-testid="message" name="message" ref={register({ required: false })} />
         </div>
         {data && (
-          <pre style={{ textAlign: "left", color: "white" }}>
+          <pre role="json-response-print" style={{ textAlign: "left", color: "white" }}>
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+        <input data-testid="submit-button" type="submit" />
       </form>
     </div>
   );
